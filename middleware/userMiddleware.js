e98484bc14913c8ae59eee63fbb50bb24b3e6366
Attah ifeanyichukwu sixtus <attah.ifeanyichukwu@verdantdevcore.com>
2025-05-20 09:12:40 -0700

    I added a message on the middleware explaining what it does

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+// this Validates MongoDB ObjectId in URL; and also sends 400 if invalid, else calls next().
 const validateObjectId = (req, res, next) => {
   const isValid = mongoose.Types.ObjectId.isValid(req.params.id);
   if (!isValid) {
