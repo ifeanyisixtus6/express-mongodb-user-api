2765eb7730ca1709cf78969e838b94adf810178f
Attah ifeanyichukwu sixtus <attah.ifeanyichukwu@verdantdevcore.com>
2025-07-22 08:45:48 -0700

    i added the userMiddleware thats validateObjectId

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './route/userRoute.js';
 import blogRoutes from "./route/blogRoute.js"
 import loginRoutes from "./route/authRoute.js"
+import validateObjectId from './middleware/userMiddleware.js';
 
 dotenv.config();
 database();
@@ -13,16 +14,7 @@ app.use(express.json());
 app.use('/api', userRoutes);
 app.use('/api', blogRoutes);
 app.use('/api', loginRoutes);
-
-// Error handling middleware 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    error: {
-      message: err.message || "Internal Server Error"
-    }
-  });
-});
+app.use(validateObjectId)
 
 const PORT = process.env.PORT || 3000;
 
