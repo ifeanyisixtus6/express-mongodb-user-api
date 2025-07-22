fb886de9928bb647eb702900c864d60f7d48ccb2
Attah ifeanyichukwu sixtus <attah.ifeanyichukwu@verdantdevcore.com>
2025-07-22 08:37:11 -0700

    i added the userMiddleware in the respectively endpoint

--- route/blogRoute.js
+++ route/blogRoute.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { createBlog, getBlogs, getBlogById, getMyBlog, updateBlogById, deleteBlogById} from "../controller/blogController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import validateObjectId   from "../middleware/userMiddleware.js";
+
 
 
 
@@ -9,7 +11,7 @@ const router =  express.Router();
 router.post("/blog", protect, createBlog)
 router.get("/blog", getBlogs)
 router.get("/blog/my-blog", protect, getMyBlog)
-router.get("/blog/:id", getBlogById)
-router.patch("/blog/:id/",  protect, updateBlogById)
+router.get("/blog/:id", validateObjectId, getBlogById)
+router.patch("/blog/:id/",  protect, validateObjectId, updateBlogById)
 router.delete("/blog/:id", protect, deleteBlogById)
 export default router
