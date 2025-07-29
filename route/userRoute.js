79939eb01c9da29a96fe4bebb9bbf94b387a4518
Attah ifeanyichukwu sixtus <attah.ifeanyichukwu@verdantdevcore.com>
2025-07-29 03:41:36 -0700

    i added validateObjectId in delete route

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {getAllUsers, getUserById, updateUserById, deleteUserById } from '../controller/userController.js';
+import {getAllUsers, getUserById, updateUserById, softDeleteUser, deleteUserById } from '../controller/userController.js';
 import { protect, authorizeRoles } from '../middleware/authMiddleware.js';
 import validateObjectId from '../middleware/userMiddleware.js';
+
 
 const router = express.Router();
 
@@ -12,8 +13,9 @@ router.get('/users', protect, authorizeRoles("admin"), getAllUsers);
 
 // Self or admin 
 router.get('/users/:id', protect, validateObjectId, getUserById);
+router.patch('/users/:id/delete', protect, validateObjectId, softDeleteUser)
 router.put('/users/:id', protect, validateObjectId,  updateUserById);
-router.delete('/users/:id', protect, deleteUserById);
+router.delete('/users/:id', protect, validateObjectId, deleteUserById);
 
 
 export default router;
